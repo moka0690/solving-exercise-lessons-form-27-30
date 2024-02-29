1cbbbeeb7a7d3cbcf57aadb3625ad0e0ab66df6e
moka0690 <madradz39@gmail.com>
2024-02-29 22:49:56 +0100

    solveSolving exercise 2

--- mine.js
+++ mine.js
@@ -6,3 +6,14 @@ console.log(userName[0].toLowerCase()); // e
 console.log(userName.slice(0,1).toLowerCase()); // e
 console.log(userName.charAt(0).repeat(3).toLowerCase()); // eee
 console.log(userName.substr(0,1).toLowerCase()); // e
+
+
+
+let word = "Elzero";
+let letterZ = "z";
+let letterE = "e";
+let letterO = "O";
+
+console.log(word.includes(letterZ)); // True
+console.log(word.toLowerCase().startsWith( letterE)); // True
+console.log(word.toUpperCase().endsWith(letterO)); // True"
